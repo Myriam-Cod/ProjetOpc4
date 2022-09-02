1664afa602a85dbed2d3c177c5b11e5efc163d84
Myriam <yria.code@gmail.com>
2022-09-02 18:30:23 +0200

    retrait des variables, while, comptabilisation de la fonction main

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,38 +7,16 @@ import java.io.FileWriter;
 public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
-		int headacheCount = 0;	// initialize to 0
-		int rashCount = 0;		// initialize to 0
-		int pupilCount = 0;		// initialize to 0
+	
 		
 		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
 
 		String line = reader.readLine();
 
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();// get another symptom
-		}
+		
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
 }
