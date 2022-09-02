a9fd328b1e1cc9074eca43c908b41de64194a988
Myriam <yria.code@gmail.com>
2022-09-02 18:23:42 +0200

    Déplacement des variables dans la méthode où elles sont utilisées

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,11 +5,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]) throws Exception {
+		int headacheCount = 0;	// initialize to 0
+		int rashCount = 0;		// initialize to 0
+		int pupilCount = 0;		// initialize to 0
 		
 		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
 
