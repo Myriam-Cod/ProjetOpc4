824625352db44a743637f95b1cfcad372d372ef0
Myriam <yria.code@gmail.com>
2022-09-03 14:42:44 +0200

    Correction dans le fichier ISymptomReader et ajout du chemin dans la variable filepath

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,5 +1,6 @@
 package com.hemebiotech.analytics;
 
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,20 +13,17 @@ import java.util.List;
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
+	private String filepath = "Project02Eclipse/symptoms.txt";
 	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
+
 	public ReadSymptomDataFromFile (String filepath) {
+	
 		this.filepath = filepath;
 	}
 	
-	@Override
-	public List<String> GetSymptoms() {
+	public List<String> GetSymptoms(){
 		ArrayList<String> result = new ArrayList<String>();
-		
+	
 		if (filepath != null) {
 			try {
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
