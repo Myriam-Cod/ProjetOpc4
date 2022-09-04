ccc950cabaef0f444a23593cf2d839fcc3b83fbf
Myriam <yria.code@gmail.com>
2022-09-04 13:25:41 +0200

    Ajout import, méthode et list

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomSort.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomSort.java
@@ -1,5 +1,15 @@
 package com.hemebiotech.analytics;
 
+import java.util.Map;
+import java.util.ArrayList;
+
+
 public class SymptomSort {
+	
+	public void fileListMap() {
+		
+		 // Create a list
+        ArrayList<String> symptomList = new ArrayList<>();
+	}
 
 }
