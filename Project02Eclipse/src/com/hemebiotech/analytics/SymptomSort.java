a073eff32dd406658f3b10b42dc8d3f8392df425
Myriam <yria.code@gmail.com>
2022-09-06 13:15:11 +0200

    Ajout commentaires dans le fichier de de trie

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomSort.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomSort.java
@@ -1,15 +1,25 @@
 package com.hemebiotech.analytics;
 
-import java.util.Map;
 import java.util.List;
 import java.util.TreeMap;
 
+/**
+ * This class initializes a Treemap type collection with the data from the list
+ * The Treemap allows to have a key value collection sorted in alphabetical order
+ * @author Myriam 
+ *
+ */
 
 public class SymptomSort {
-	
+	/**
+	 * Variable that contains the list
+	 */
 	private List<String> symptomList;
 	
-
+	/**
+	 * Constructor
+	 * @param symptomList
+	 */
 	public SymptomSort(List<String> symptomList) {
 		// TODO Auto-generated constructor stub
 		this.symptomList = symptomList;
@@ -17,19 +27,23 @@ public class SymptomSort {
 
 	
 	public TreeMap<String, Integer> fileListMap() {
-
-        // Create TreeMap
+       
 		TreeMap<String, Integer> symptomMap = new TreeMap<String, Integer>();
 		
-		//For each pour parcourir la list
+		/**
+		 * Foreach to iterate through the symptoms in the list
+		 */
         for(String theSymptom : symptomList) {
+        	/**
+        	 * Condition, if the row is null, I add the key and value 1 to my collection
+        	 * else I add the key and value++
+        	 */
         		if(symptomMap.get(theSymptom) == null) {
         			symptomMap.put(theSymptom, 1);
         		}else {	
         			symptomMap.put(theSymptom, symptomMap.get(theSymptom)+1);
         		}
         }
-        System.out.println(symptomMap);
         return symptomMap;
 	}
 
