176580a4f62d705d7c0e451b54da8e15cb43303d
Myriam <yria.code@gmail.com>
2022-09-06 13:23:03 +0200

    Ajout commentaires dans le fichier Writer

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomWriter.java
@@ -1,14 +1,29 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.TreeMap;
 
+/**
+ * This class generates a file from a Treemap collection
+ * @author Myriam 
+ *
+ */
 public class SymptomWriter {
+	/**
+	 * Variable that contains the file path sort
+	 */
 	private String writerFile;
+	/**
+	 * Variable that contains the Treemap collection
+	 */
 	private TreeMap<String, Integer> symptomMapList;
 	
+	/**
+	 * Constructor
+	 * @param symptomMapList
+	 * @param writerFile
+	 */
 	public SymptomWriter(TreeMap<String, Integer> symptomMapList, String writerFile) {
 		this.symptomMapList = symptomMapList;
 		this.writerFile = writerFile;
@@ -18,8 +33,11 @@ public class SymptomWriter {
 		
 		try {
 			FileWriter writer = new FileWriter (writerFile);
+			
+			/**
+			 * Foreach which loops through the treemap collection and writes each line to the file
+			 */
 			for(String key : symptomMapList.keySet()) {
-				System.out.println(key + " : " + symptomMapList.get(key));
 				writer.write(key + " : " + symptomMapList.get(key) + "\n");
 			}
 			writer.close();
